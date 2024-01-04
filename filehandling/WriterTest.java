c79f19cc57f3d43d36b482fe615e02e8962848e9
saphaltaneupane <neupanesaphalta@gmail.com>
2024-01-04 14:32:55 +0545

    ok

--- filehandling/WriterTest.java
+++ filehandling/WriterTest.java
@@ -1,6 +1,15 @@
 package filehandling;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriterTest {
     //FileWrite: write txt format only
-    //
+    //FileReader: read
+    public static void main(String[] args) throws IOException {
+        FileWriter file = new FileWriter("e://product.txt");
+        file.write("id=4444\n");
+        file.close();
+
+    }
 }
